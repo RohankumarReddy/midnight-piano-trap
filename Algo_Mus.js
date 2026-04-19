e4ffa052cc04e9aabb6bc326457f0b83fd726039
Nagaruru Rohankumar Reddy <150910334+RohankumarReddy@users.noreply.github.com>
2026-04-19 17:28:15 +0530

    Added a .js  and a .wav file
    .js contains the actual code of the beat and  AlgoMusic.wav is the final beat

---
+++ Algo_Mus.js
@@ -0,0 +1,12 @@
+stack(
+     
+sound("bd*2 hh*2/2 sd/1 oh bd*2 hh*2 sd casio:5")
+    .bank("linndrum"),
+    note("{[1*2]  [2*2] [3*2] [4]} ")
+    .speed(2)
+    .scale("a:minor")
+    .sound("piano")
+   
+   
+  
+)
